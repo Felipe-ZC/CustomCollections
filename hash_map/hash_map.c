51340a1e233e6d2efed4da68b7276f3accc5584d
Felipe-ZC <fzunigacar@gmail.com>
2019-09-18 10:39:33 -0700

    Refactoring hash map

--- hash_map/hash_map.c
+++ hash_map/hash_map.c
@@ -13,19 +13,28 @@ int default_hash(char* key)
 }
 
 // Initialize a new instance of hash_map...
-hash_map* init(int (*hash_ptr)(char *key))
+/*hash_map* init_w_hash(int (*hash_ptr)(char *key))
 {
     hash_map *new_map = (hash_map*)malloc(sizeof(hash_map));
 	new_map->hash = (hash_ptr != NULL) ? hash_ptr : &default_hash;
 	new_map->map = malloc(sizeof(void*) * BUFSIZ);
 	new_map->length = BUFSIZ;
  	return new_map;
+}*/
+
+hash_map* init()
+{
+    hash_map *new_map = (hash_map*)malloc(sizeof(hash_map));
+    new_map->hash = &default_hash;
+    new_map->map = malloc(sizeof(void*) * BUFSIZ);
+    new_map->length = BUFSIZ;
+    return new_map;
 }
 
 void dealloc_map(hash_map *h_map)
 {
-	free(h_map->map);
-	free(h_map);
+    free(h_map->map);
+    free(h_map);
 }
 
 // TODO: Check if there is already a value for the given key...
@@ -33,32 +42,12 @@ void dealloc_map(hash_map *h_map)
 // aka let the caller handle that...
 void insert(char *key, void *value, hash_map *h_map)
 {		
-	int index = h_map->hash(key);
-	if(index >= h_map->length)
-	   	index %= (h_map->length - 1);
-	// TODO: This only points to value
-	h_map->map[index] = value;
+    int index = h_map->hash(key);
+    if(index >= h_map->length) index %= (h_map->length - 1);
+    h_map->map[index] = value; // TODO: This only points to value
 }
 
 void *get(char *key, hash_map *h_map)
 {
-	return h_map->map[h_map->hash(key)];	
-}
- 
-// Use this function for testing only...
-int main()
-{	
-	hash_map *test = init(NULL);
-	
-	char test_keys[3][30] = {"pipo", "yes", "hello"};
-	char test_vals[3][30] = {"papo", "no", "goodbye"};
-	
-	int i;
-	for(i = 0; i < 3; ++i) 
-		insert(*((test_keys) + i), *((test_vals) + i), test);
-	for(i = 0; i < 3; ++i) 
-		printf( "key: %s, value: %s\n", *(test_keys + i), (char*)(get(*((test_keys) + i), test)) );
-
-	dealloc_map(test);
-    return 0;
-}
+    return h_map->map[h_map->hash(key)];	
+} 
