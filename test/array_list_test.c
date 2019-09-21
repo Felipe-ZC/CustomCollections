8f3e764d872b2fc1dbc82241252dba83fb844a02
Felipe-ZC <fzunigacar@gmail.com>
2019-09-21 12:46:34 -0700

    Implementing test functions for each function defined in array_list.h

--- test/array_list_test.c
+++ test/array_list_test.c
@@ -2,30 +2,42 @@
 #include <stdint.h>
 #include "../array_list/array_list.h"
 
-int main()
+int test_insert(array_list *my_list)
 {
-	array_list *myList = init();
 	int i;
 	
-	printf("The size of the list is: %d\n", myList->size);	
-	printf("The capacity of the list is: %d\n", myList->capacity);
+	printf("The size of the list is: %d\n", my_list->size);	
+	printf("The capacity of the list is: %d\n", my_list->capacity);
 	
 	printf("-------------- Populating list --------------\n");
-	for(i = 0; i < myList->capacity; ++i) insert((void*)(intptr_t)i, myList);
+	for(i = 0; i < (my_list->capacity); ++i)
+	   	insert((void*)(intptr_t)i, my_list);
 	printf("-------------- Done Populating list --------------\n");
 	
-	printf("The size of the list is: %d\n", myList->size);	
-	printf("The capacity of the list is: %d\n", myList->capacity);
+	printf("The size of the list is: %d\n", my_list->size);	
+	printf("The capacity of the list is: %d\n", my_list->capacity);
 	printf("Inserting one more element...\n");
+	insert((void*)(intptr_t)69420, my_list);
+	printf("The size of the list is: %d\n", my_list->size);	
+	printf("The capacity of the list is: %d\n", my_list->capacity);
+
+	printf("-------------- Populating list --------------\n");
+	for(i = (my_list->size); i < (my_list->capacity); ++i)
+	   	insert((void*)(intptr_t)i, my_list);
+	printf("-------------- Done Populating list --------------\n");
 	
-	insert((void*)(intptr_t)69420, myList);
-	
-	printf("The size of the list is: %d\n", myList->size);	
-	printf("The capacity of the list is: %d\n", myList->capacity);
-	
-	for(i = 0; i < myList->size; ++i)	printf("%d\n", (int*)get(i, myList));
-	
-	dealloc_alist(myList);
-			
+	printf("Inserting one more element...\n");
+	insert((void*)(intptr_t)69420, my_list);
+	printf("The size of the list is: %d\n", my_list->size);	
+	printf("The capacity of the list is: %d\n", my_list->capacity);
+
+}
+
+
+int main()
+{
+	array_list *list = init();
+	test_insert(list);	
+	dealloc_alist(list);
 	return 0;
 }
