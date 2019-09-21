1c6e933b53336cd294b66d48f511b8291817ea6f
Felipe-ZC <fzunigacar@gmail.com>
2019-09-21 11:29:04 -0700

    Finished implementing functions in arraylist.h

--- array_list/array_list.c
+++ array_list/array_list.c
@@ -3,50 +3,55 @@
 #include <string.h>
 #include "array_list.h"
 
-// TODO: Dont give users access to the array_list directly!
-// Declare it as static in this file so it can only be used
-// by methods defined in this file!
 array_list *init() 
 {   
 	array_list *new_list = (array_list*)malloc(sizeof(array_list));
-	new_list->list = malloc(sizeof(void*)*BUFSIZ);
+	new_list->capacity = BUFSIZ;
+	new_list->list = malloc(sizeof(void*) * new_list->capacity);
 	new_list->size = 0;
 	return new_list;
 }
 
 void dealloc_alist(array_list *a_list)
 {
-	int i;
-	// TODO: Add another function that deallocs all the items 
-	// inside of list, let user know its unsafe.
-	/*for(i = 0; i < (a_list->size); ++i) free(a_list->list[i]);*/
 	free(a_list->list);
 	free(a_list);
 }
 
+int resize(array_list *a_list)
+{	
+	// Increase capacity by BUFISZ...
+	a_list->capacity *= BUFSIZ;
+	a_list->list = realloc(a_list->list, sizeof(void*) * (a_list->capacity));	
+	return a_list->capacity;
+}
+
 // Append a new element to the end of a_list.
-void insert(void *value, array_list *a_list)
+int insert(void *value, array_list *a_list)
 {
-	if(a_list->size >= 0) {
+	if(a_list->size < a_list->capacity)
+   	{
 		a_list->list[(a_list->size)] = value;
 		a_list->size++;
-	} // else return -1 
+		return 0;
+	}
+   	else return resize(a_list); 
+}
+
+void* del(int index, array_list *a_list)
+{	
+	if(index < a_list->size)
+   	{
+		int i = 0;
+		while(i < index) ++i;
+		void *tmp = a_list->list[i];
+		a_list->list[i] = NULL;
+		return tmp;
+	}
+   	else return NULL;
 }
 
 void *get(int index, array_list *a_list)
 {
     return (a_list->size > index) ? a_list->list[index] : NULL;
-}
-
-int main() {
-	array_list *test = init(1);
-	char *dyna_str = (char*)malloc(sizeof(char*));
-	strcpy(dyna_str, "swag");
-	insert("yolo", test);
-	insert(dyna_str, test);
-	printf("%s\n", (char*)(test->list[0]));
-	printf("The length of the test array is %d\n", test->size);
-        free(dyna_str);
-	dealloc_alist(test);
-	return 0;
 }
--- array_list/array_list.h
+++ array_list/array_list.h
@@ -1,12 +1,13 @@
 struct _list {
     void ** list;
-    int size; 
+    int size; // Number of elements in list
+    int capacity; // Total number of elements that can be currently held in list
 } typedef array_list;
 
 array_list *init();
-void *del(int);
-void *resize(array_list*);
+void *del(int, array_list*);
+int resize(array_list*);
 void *get(int, array_list*);
-void insert_at(int, array_list*);
-void insert(void*, array_list*);
+int insert_at(int, array_list*);
+int insert(void*, array_list*);
 void dealloc_alist(array_list*);
